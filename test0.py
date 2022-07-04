cf6f9d4fd680902f38f715add1cf622408c94bce
fabiobillo <100129085+fabiobillo@users.noreply.github.com>
2022-07-04 12:47:36 +0200

    Add File test0

---
+++ test0.py
@@ -0,0 +1,36 @@
+
+#Name= input ("inserisci il nome")
+#errore= "non hai inserito un valore"
+#if Name !=0:
+ #   print (Name)
+
+
+#Name= input ("inserisci il nome")
+#cognome= input("inserisci il cognome")
+#Luogo=input("inserisci luogo residenza")
+#Lista=[]
+#if Name != False:
+    #Lista.append (Name)
+
+#if cognome != False:
+ #   Lista.append (cognome)
+
+#if Luogo !=False:
+ #   Lista.append (Luogo)
+
+#print(Lista)    
+
+Name= input ("inserisci il nome")
+cognome= input("inserisci il cognome")
+Luogo=input("inserisci luogo residenza")
+Lista=[]
+if Name != "fabio":
+    Lista.append (Name)
+
+if cognome != "billeri":
+    Lista.append (cognome)
+
+if Luogo !="Roma":
+    Lista.append (Luogo)
+
+print(Lista)  
