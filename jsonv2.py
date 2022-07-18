14a96e13a3a4729067e06d55916160f92c42ede8
fabiobillo <100129085+fabiobillo@users.noreply.github.com>
2022-07-18 14:07:48 +0200

    add version2 of json app

---
+++ jsonv2.py
@@ -0,0 +1,39 @@
+import json
+
+listaCandidati=[]
+
+scelta_json=""
+while scelta_json!="end":
+    print("che vosa vuoi fare?")
+    print("1. Caricare")
+    print("2. visualizzare")
+    print("end per uscire")
+    scelta_json=input("")
+    if scelta_json=="1":
+
+        scelta="si"
+        while scelta=="si":
+            nome=input("inserisci il nome del candidato")
+            listaCandidati.append(nome)
+            print(listaCandidati)
+            if len(listaCandidati) >0:
+                y=json.dumps(listaCandidati)
+                scelta= input("vuoi inserire altri candiadati? si/no")
+            else:
+                print("non hai inserito dati")    
+                scelta= input("vuoi provare ad inserire altri candiadati con valori corretti? si/no")
+    elif scelta_json=="2":
+        print("questi sono i candidati:")
+        print(listaCandidati)
+        scelta="si"
+        while scelta=="si":
+            if len(listaCandidati) >0:
+                y=json.dumps(listaCandidati)
+                z=json.loads(y)
+                scelta_visualizzazione= int(input("quanti candidati vuoi visualizzare?"))
+                for n in range(scelta_visualizzazione):
+                    print(z[n])    
+                break
+            else:
+                print("non hai inserito dati")    
+                scelta= input("vuoi provare ad inserire altri candiadati con valori corretti? si/no")
